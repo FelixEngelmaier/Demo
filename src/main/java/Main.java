80f4dcd510c9598e17b7ff37b8938d9c83e12afd
DESKTOP-PU250QC\FelixPC <wi20b031@technikum-wien.at>
2021-05-16 13:07:00 +0200

    Maven Setup finished, demo main erstellt, unitests geschrieben

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static String s = "Hallo";
+    public static void main (String[] args){
+
+        System.out.println("Hi");
+    }
+    public static String getString() {
+        return s;
+
+    }
+}
---
+++ src/test/java/Main_Test.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Main_Test {
+    @Test
+    public void test_given_applicationIsStartet_when_MainGetMessageIsCalled_then_correctOutputIsExpected(){
+        //Tripple A (Act, Arrange, Assert)
+        //Act zb Etwas initiallisieren, variable etc.
+        //Arrange
+        String Actuall = Main.getString();
+        //Assert
+        Assertions.assertEquals("Hallo",Actuall);
+    }
+}
